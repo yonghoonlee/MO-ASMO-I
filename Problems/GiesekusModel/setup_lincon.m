a2650de07d09f1c6db9ed092550b72e59ee164f0
Yong Hoon Lee <yonghoonlee@outlook.com>
2017-10-18 00:45:17 -0500

    Before major change, delete previous version from the repository

--- Problems/GiesekusModel/setup_lincon.m
+++
@@ -1,9 +0,0 @@
-function [A,b,Aeq,beq] = setup_lincon()
-    A = [];
-    b = [];
-    Aeq = [];
-    beq = [];
-    
-    
-    addAttachedFiles(gcp,{'dhat.m','fd_weights_full.m','Giesekus_2M3D_v002.m','semhat.m','zwgll.m'});
-end
--- README.md
+++
@@ -1,5 +0,0 @@
-# MO-ASMO-I
-Multiobjective Adaptive Surrogate Modeling-based Optimization Toolbox I
-
-## Usage:
-Modify the main script: *main.m* file, and run.
